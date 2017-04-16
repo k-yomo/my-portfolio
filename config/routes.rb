97e6571b6635801350928832d1a8aafa57a3ffc2
kanji yomoda <kanji@mba.local>
2017-04-15 21:23:36 -0700

    portfolio items via Resource generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :portfolios
   get 'pages/home'
 
   get 'pages/about'
@@ -8,3 +9,4 @@ Rails.application.routes.draw do
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
