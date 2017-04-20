041d4e12df37347323816dba753db1cbfacbf116
kanji yomoda <kanji@mba.local>
2017-04-19 19:12:20 -0700

    Change action in portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,9 @@ class Portfolio < ApplicationRecord
 
   validates_presence_of :title,:body,:main_image,:thumb_image
 
-
-  scope :angular, -> { where(subtitle: "Angular") }
+  def self.angular
+    where(subtitle: 'Angular')
+  end
 
 end
 
