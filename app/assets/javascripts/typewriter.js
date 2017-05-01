25afc1818946807756369e095ae4217c7505e197
Kanij Yomoda <kanji@mbp.local>
2017-05-01 14:30:46 -0700

    Implemented typewriting for home page

---
+++ app/assets/javascripts/typewriter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    setTimeout(function(){
+        $('.typing').typeIt({
+            speed: 100,
+            lifeLike: false,
+            autoStart: false
+        })
+            .tiType("Hallo")
+            .tiPause(450)
+            .tiSettings({deleteSpeed: 130})
+            .tiDelete(4)
+            .tiType("ello,")
+            .tiPause(200)
+            .tiType(" my name is Kanji. ")
+            .tiPause(200)
+            .tiType(" I'm a Growth Hacker ")
+            .tiPause(100)
+            .tiType("living in vancouver, ")
+            .tiPause(350)
+            .tiType("BC.")
+    }, 2000);
+});
